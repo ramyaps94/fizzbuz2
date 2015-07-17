8aba0b32780238b307c5b541e00ce9aae92ce2ad
ramyaps94 <ramya.sudarshan1994@gmail.com>
2015-07-17 14:54:35 +0530

    IsFizz and IsBuzz and IsFizzBuzz working Fine

--- src/main/java/com/pathashala/Fizzbuzz.java
+++ src/main/java/com/pathashala/Fizzbuzz.java
@@ -1,30 +1,22 @@
 package com.pathashala;
 
 public class Fizzbuzz {
-      //  private int value;
-        public String IsFizzBuzzBlab(int value)
-        {
-          if(value%3==0&&value%5==0)
-          {
-                  System.out.println("FizzBuzz");
-                  return "FizzBuzz";
-          }
-          else if(value%3==0)
-          {
-             System.out.println("Fizz");
-              return "Fizz";
-          }
-          else if(value%5==0)
-          {
-                  System.out.println("Buzz");
-                  return "Buzz";
-          }
-          else
-          {
-             System.out.println("Blab");
+    //  private int value;
+    public String IsFizzBuzzBlab(int value) {
+        if (value % 3 == 0 && value % 5 == 0) {
+            System.out.println("FizzBuzz");
+            return "FizzBuzz";
+        } else if (value % 3 == 0) {
+            System.out.println("Fizz");
+            return "Fizz";
+        } else if (value % 5 == 0) {
+            System.out.println("Buzz");
+            return "Buzz";
+        } else {
+            System.out.println("Blab");
 
-          }
-            return "Blab";
         }
+        return "Blab";
+    }
 
 }
--- src/test/java/com/pathashala/FizzbuzzTest.java
+++ src/test/java/com/pathashala/FizzbuzzTest.java
@@ -8,22 +8,26 @@ public class FizzbuzzTest {
     private Fizzbuzz fizzbuzz;
 
     @Test
-    public void ShouldReturnFizzIFNumberMultipleOfThree()
-    {
-        fizzbuzz= new Fizzbuzz();
-        String result=fizzbuzz.IsFizzBuzzBlab(6);
-        assertEquals("Fizz",result);
+    public void ShouldReturnFizzIFNumberMultipleOfThree() {
+        fizzbuzz = new Fizzbuzz();
+        String result = fizzbuzz.IsFizzBuzzBlab(6);
+        assertEquals("Fizz", result);
 
     }
 
     @Test
-    public void ShouldReturnBuzzIFNumberMultipleOfFive()
-    {
-        fizzbuzz= new Fizzbuzz();
-        String result=fizzbuzz.IsFizzBuzzBlab(10);
-        assertEquals("Buzz",result);
+    public void ShouldReturnBuzzIFNumberMultipleOfFive() {
+        fizzbuzz = new Fizzbuzz();
+        String result = fizzbuzz.IsFizzBuzzBlab(10);
+        assertEquals("Buzz", result);
 
     }
+    @Test
+    public void ShouldReturnBuzzIFNumberMultipleOfThreeFive() {
+        fizzbuzz = new Fizzbuzz();
+        String result = fizzbuzz.IsFizzBuzzBlab(15);
+        assertEquals("FizzBuzz", result);
 
+    }
 
 }
